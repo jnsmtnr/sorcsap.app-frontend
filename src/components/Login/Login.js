cea60c1d66525696e77a952819bf039acc1663c3
jnsmtnr <janosmaitner@gmail.com>
2021-08-16 18:52:41 +0200

    validate email

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,35 +5,47 @@ function Login(props) {
   const email = useRef()
   const password = useRef()
   const [hasError, setHasError] = useState(false)
+  const [invalidEmail, setInvalidEmail] = useState(false)
 
   const history = useHistory()
+
+  function validateEmail() {
+    const enteredEmail = email.current.value
+    return enteredEmail.includes('.') && enteredEmail.includes('@')
+  }
 
   function handleSubmit(event) {
     event.preventDefault()
 
     setHasError(false)
+    setInvalidEmail(false)
 
-    fetch('http://localhost:8000/users/login', { 
+    if (!validateEmail()) {
+      setInvalidEmail(true)
+      return
+    }
+
+    fetch('http://localhost:8000/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
-      }, 
+      },
       body: JSON.stringify({
         email: email.current.value,
         password: password.current.value
       }),
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-      return response.json()
-    })
-    .then(data => {
-      props.onSuccesfulLogin(data.token)
-      history.replace('/test')
-    })
-    .catch(() => setHasError(true))
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        return response.json()
+      })
+      .then(data => {
+        props.onSuccesfulLogin(data.token)
+        history.replace('/test')
+      })
+      .catch(() => setHasError(true))
   }
 
   return (
@@ -44,6 +56,7 @@ function Login(props) {
         className="border p-1"
         ref={email}
       />
+      {invalidEmail && <span className="text-red-400 text-sm">Rossz e-mail cím</span>}
       <label htmlFor="password">Jelszó</label>
       <input
         id="password"
