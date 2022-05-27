0b4721ba2f99dfab9af6085dbd79585c271bdd44
jnsmtnr <janosmaitner@gmail.com>
2022-05-27 20:26:42 +0200

    fix test

--- src/pages/Hello.test.js
+++ src/pages/Hello.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import Hello from './Hello'
 
 test('renders app name and beta text', () => {
-  render(<Hello />)
+  render(
+    <BrowserRouter>
+      <Hello />
+    </BrowserRouter>
+  )
   const appName = screen.getByText((content, element) => element.textContent === 'SÖRCSAPP')
   expect(appName).toBeInTheDocument()
   const beta = screen.getByText((content, element) => element.textContent === 'Béta')
