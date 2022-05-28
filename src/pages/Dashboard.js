b0e3127d469aaae71575b4ff25c4b3058b5a6476
jnsmtnr <janosmaitner@gmail.com>
2022-05-28 09:31:50 +0200

    add style to dashboard links

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
     return (
-        <div className="text-center">
-            <Link to="/rate-beer" className="block">Ittam egy sört</Link>
-            <Link to="/my-ratings" className="block">Ezeket ittam már</Link>
+        <div className="flex flex-col items-stretch space-y-4">
+            <Link to="/rate-beer" className="bg-yellow-600 hover:bg-yellow-500 px-4 py-2 text-white rounded-full text-xl text-center">Ittam egy sört</Link>
+            <Link to="/my-ratings" className="bg-yellow-600 hover:bg-yellow-500 px-4 py-2 text-white rounded-full text-xl text-center">Ezeket ittam már</Link>
         </div>
     )
 }
